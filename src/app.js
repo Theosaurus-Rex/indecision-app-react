ad9e5a3ea8c68f93b8705aa6942983f51a717ec1
Theosaurus-Rex <theosaurus13@gmail.com>
2021-06-11 10:47:56 +1000

    refactor to use components

--- src/app.js
+++ src/app.js
@@ -1,61 +1,68 @@
-console.log('App.js is running!');
-
-const app = {
-  title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer',
-  options: []
-};
-
-const onFormSubmit = (e) => {
-  e.preventDefault();
-
-  const option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    render();
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Action/>
+        <Options/>
+        <AddOption/>
+      </div>
+    )
   }
-};
-
-const onRemoveAll = () => {
-  app.options = [];
-  render();
-};
-
-const onMakeDecision = () => {
-  const randomNum = Math.floor(Math.random() * app.options.length)
-  const option = app.options[randomNum]
-  alert(option)
-  console.log(randomNum)
 }
 
-const appRoot = document.getElementById('app');
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision </h1>
+        <h2>Put your life in the hands of the computer!</h2>
+      </div>
+    )
+  }
+}
 
-const render = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-      <button onClick={onMakeDecision} disabled={app.options.length === 0}>What should I pick?</button>
-      <button onClick={onRemoveAll}>Remove All</button>
-      
-      <ol>
-      {
-        app.options.map((option) => {
-          return <li key={option}>{option}</li>
-        })
-      }
-      </ol>
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option" />
-        <button>Add Option</button>
-      </form>
-    </div>
-  );
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I pick?</button>
+      </div>
+    )
+  }
+}
 
-  ReactDOM.render(template, appRoot);
-};
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        <Option/>
+        <Option/>
+        <Option/>
+      </div>
+    )
+  }
+}
 
-render();
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>AddOption Component Here</h3>
+      </div>
+    )
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return(
+      <div>
+        <h3>Option Component Here</h3>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<IndecisionApp/>, document.getElementById("app"))
